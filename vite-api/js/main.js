5d0832761cad7110d902d624a49a3095c3b5c82a
GhTmp1A <stanbutter4@gmail.com>
2022-12-23 13:46:38 -0500

    T3 done

--- vite-api/js/main.js
+++ vite-api/js/main.js
@@ -36,4 +36,17 @@ suzie.then((result) => {
 });
  */
 
-api.quotable.io / random;
+const URL = "https://api.quotable.io/random";
+
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    document.getElementById("api-response").textContent = data.content;
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getData(URL);
