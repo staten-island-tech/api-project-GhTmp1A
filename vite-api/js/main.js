052bc76f5bbde56601e7135a4250846000ec07dd
GhTmp1A <stanbutter4@gmail.com>
2023-01-04 13:59:22 -0500

    Figure out dog pictures

--- vite-api/js/main.js
+++ vite-api/js/main.js
@@ -58,11 +58,22 @@ async function getquote(URL) {
   DOMSelectors.display.innerHTML = "";
   try {
     const response = await fetch(URL);
-    const data = await response.json();
+    if (response.status < 200 || response.status > 299) {
+      console.log(response.status);
+      throw error(response);
+    } else {
+      const data = await response.json();
+      document.getElementById("api-response").textContent = data.name;
+      console.log(data);
+    }
+    /*    const data = await response.json();
     document.getElementById("api-response").textContent = data.content;
-    console.log(data);
+    console.log(data); */
   } catch (error) {
     console.log(error);
+    console.log("Get outta here fix your code");
+    document.getElementById("api-response").textContent =
+      "Sorry boss, on my break right now";
   }
 }
 
@@ -70,11 +81,19 @@ async function getdog(URL) {
   DOMSelectors.display.innerHTML = "";
   try {
     const response = await fetch(URL);
-    const data = await response.json();
-    document.getElementById("api-response").textContent = data.content;
-    console.log(data);
-    DOMSelectors.img.setAttribute("src", data.)
+    if (response.status < 200 || response.status > 299) {
+      console.log(response.status);
+      throw error(response);
+    } else {
+      const data = await response.json();
+      document.getElementById("api-response").textContent = data.content;
+      console.log(data);
+    }
+    DOMSelectors.img.setAttribute("src", img);
   } catch (error) {
     console.log(error);
+    console.log("Get outta here fix your code");
+    document.getElementById("api-response").textContent =
+      "Out today. Back tommorow.";
   }
 }
